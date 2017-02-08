1961a938545244f53a49f9993629592d9bb264c5
Anthony Rey <anthony.rey5@gmail.com>
2017-02-08 11:59:29 +0100

    Added dying ants

---
+++ XMLParser/AntSimulator/Unit/AntCorpse.cs
@@ -0,0 +1,21 @@
+﻿using F2J2A.CommonSimulator.Core;
+using F2J2A.CommonSimulator.Core.Unit;
+using Microsoft.Xna.Framework;
+
+namespace F2J2A.AntSimulator.Unit
+{
+    public class AntCorpse : GameUnit
+    {
+
+        public AntCorpse(int x, int y) : base(x, y)
+        {
+            Texture2D = Simulator.Instance.Textures["antcorpse"];
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            Simulator.Instance.SpriteBatch.Draw(Texture2D, new Vector2(X * 32, Y * 32));
+        }
+    }
+}
+
