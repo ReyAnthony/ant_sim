b23443d5b410a09ab0b898eb4837894c61848d94
Anthony Rey <anthony.rey5@gmail.com>
2017-02-08 15:08:09 +0100

    Added dying ants

---
+++ XMLParser/AntSimulator/AI/Command/ClearCorpses.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using F2J2A.AntSimulator.Unit;
+using F2J2A.CommonSimulator.Core.AI;
+
+namespace F2J2A.AntSimulator.AI.Command
+{
+    public class ClearCorpses : ICommand
+    {
+        private readonly List<AntCorpse> _corpses;
+        private readonly List<AntCorpse> _deletedCorpses;
+
+        public ClearCorpses(List<AntCorpse> corpses)
+        {
+            _corpses = corpses;
+            _deletedCorpses = new List<AntCorpse>();
+        }
+
+        public void Execute()
+        {
+            _deletedCorpses.AddRange(_corpses);
+            _corpses.Clear();
+        }
+
+        public void Undo()
+        {
+           _corpses.AddRange(_deletedCorpses);
+           _deletedCorpses.Clear();
+        }
+    }
+}
---
+++ XMLParser/CivSim/Unit/City.cs
@@ -0,0 +1,22 @@
+﻿using F2J2A.CommonSimulator.Core;
+using F2J2A.CommonSimulator.Core.Unit;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace XMLParser.CivSim.Unit
+{
+    public class City : GameUnit
+    {
+        private Texture2D _texture2D;
+
+        public City(int x, int y) : base(x, y)
+        {
+            _texture2D = Simulator.Instance.Textures["ant"];
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+
+        }
+    }
+}
